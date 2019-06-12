99b73075fd39e0db9ee1491b091227bbd9ba1f49
Artiya Thinkumpang <artiya4u@gmail.com>
2019-06-12 12:36:38 +0700

    Change output to Indesign format.

--- GenPromoCode.js
+++ GenPromoCode.js
@@ -12,11 +12,9 @@ const infuraProvider = network => providerWithMnemonic(
   `https://${network}.infura.io/v3/96bfc78effaa4a32bf99ce0dd4453132`,
 );
 
-const network = process.env.NETWORK || 'rinkeby';
+const network = process.env.NETWORK || 'mainnet';
 const provider = infuraProvider(network);
 let w3 = new Web3(provider);
-console.log('Promo Code Generator...');
-console.log('Owner Address:', provider.address);
 const amount = new w3.utils.BN('88000000000000000000');
 const numberOfCode = 300;
 const approveAmount = amount.mul(new w3.utils.BN(numberOfCode));
@@ -28,38 +26,39 @@ if (network === 'mainnet') {
 }
 const tokenContractAddress = '0xd42debE4eDc92Bd5a3FBb4243e1ecCf6d63A4A5d';
 
-let i = 0;
+let i = 1;
 let code = '';
 let signature = '';
 let privateKey = bufferToHex(provider.wallet._privKey);
-console.log('==========================================  CODE  ==========================================');
-for (i; i <= numberOfCode; i++) {
-  code = 'C819' + Math.abs(Math.random() * 1000000).toFixed(0).padStart(6, '0');
+console.log('#QRcodes');
+for (i; i <= numberOfCode + 1; i++) {
+  code = 'C8190612' + i.toFixed(0).padStart(3, '0');
   let sign = w3.eth.accounts.sign(w3.utils.fromUtf8(code), privateKey);
   signature = sign.signature;
   if (i < numberOfCode) {
-    let link = `https://carboneum.io/p/?c=${code}&s=${signature}`;
+    let link = `URL:https://carboneum.io/p/?c=${code}&s=${signature}`;
     console.log(link);
   }
 }
-console.log('============================================================================================');
-console.log('Approving contract spending...');
+
+console.error(provider.address);
+console.error('Approving contract spending...');
 const token = new w3.eth.Contract(TokenABI, tokenContractAddress);
 token.methods.approve(promocodeAddress, w3.utils.toHex(approveAmount)).send(
   {from: provider.address, gas: 300000}).on('transactionHash', (hash) => {
-  console.log('Approving Tx:', hash);
-  console.log('Testing Redeem Code...', code);
+  console.error('Approving Tx:', hash);
+  console.error('Testing Redeem Code...', code);
   const promoCode = new w3.eth.Contract(PromoCodeABI, promocodeAddress);
   promoCode.methods.redeem(code, signature).send(
     {from: provider.address, gas: 300000}).on('transactionHash', (redeemHash) => {
-    console.log('Test Redeem Tx:', redeemHash);
+    console.error('Test Redeem Tx:', redeemHash);
     process.exit();
   }).on('confirmation', (confirmationNumber, receipt) => {
-    console.log('confirmation2');
+    console.error('confirmation2');
   }).on('receipt', (receipt) => {
   }).on('error', console.error);
 }).on('confirmation', (confirmationNumber, receipt) => {
-  console.log('confirmation1');
+  console.error('confirmation1');
 }).on('receipt', (receipt) => {
-  console.log('receipt1');
+  console.error('receipt1');
 }).on('error', console.error);
